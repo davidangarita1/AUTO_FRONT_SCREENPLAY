ff1df6acc6611f38944070677b9a6996a03800da
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-19 22:12:26 -0500

    refactor(runners): migrate CucumberTestRunner to JUnit 5 suite configuration

--- src/test/java/com/turnos/runners/CucumberTestRunner.java
+++ src/test/java/com/turnos/runners/CucumberTestRunner.java
@@ -1,14 +1,15 @@
 package com.turnos.runners;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"com.turnos.stepdefinitions", "com.turnos.hooks"},
-        plugin = {"pretty"}
-)
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
 public class CucumberTestRunner {
 }
